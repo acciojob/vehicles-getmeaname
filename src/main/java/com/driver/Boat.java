423df1a6c4c90a800e2ae7f6263b545c360336c4
Arun <arun.idk@outlook.com>
2025-04-17 14:49:37 +0530

    Completed Task 2

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,23 @@
+package com.driver;
+
+public abstract class Boat implements WaterVehicle{
+    String name;
+    int capacity;
+
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    // Implement getVehicleName method
+    @Override
+    public String getVehicleName() {
+        return name;
+    }
+
+    // Implement getVehicleCapacity method
+    @Override
+    public int getVehicleCapacity() {
+        return capacity;
+    }
+}
--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -11,15 +11,47 @@ public class Car extends Vehicle {
 
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
         //Hint: Car extends Vehicle
+        super(name);
+        this.currentGear = 1;
+        this.doors = doors;
+        this.type = type;
+        this.wheels = wheels;
+        this.seats = seats;
+        this.gears = gears;
+        this.isManual = isManual;
     }
 
     public void changeGear(int newGear){
-
-        System.out.println("changeGear method called - The gear is changed to: " + currentGear);
+        this.currentGear = newGear;
+        System.out.println("changeGear method called - The gear is changed to: " + this.currentGear);
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
-
+        this.move(newSpeed, newDirection);
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
+    }
+
+    public int getWheels() {
+        return this.wheels;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public int getDoors() {
+        return this.doors;
+    }
+
+    public int getGears() {
+        return this.gears;
+    }
+
+    public boolean isManual() {
+        return this.isManual;
+    }
+
+    public int getCurrentGear() {
+        return this.currentGear;
     }
 }
